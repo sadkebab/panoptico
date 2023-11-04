730fb4ce3fda6c8587f1f9391897bf0aaf33a235
Alberto Harka <alb.harka@icloud.com>
2023-11-04 14:25:09 +0400

    minor change on api/data

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, userAgent } from "next/server";
 
 export const GET = async (request: NextRequest) => {
-  const ip = request.ip
+  const ip = request.ip || "undef"
   const geo = request.geo
   const ua = userAgent(request)
   return new Response(JSON.stringify({ ip, geo, ua }), { status: 200 })
